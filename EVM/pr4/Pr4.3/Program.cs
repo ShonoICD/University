a46af2d1d9b46121d101c554dc9cd12fb3731069
SoHappyICD <danny.zubairaev@gmail.com>
2023-12-21 12:55:44 +0500

    Practica 4

---
+++ EVM/pr4/Pr4.3/Program.cs
@@ -0,0 +1,16 @@
+﻿using Iot.Device.OneWire;
+
+bool flag = true;
+Console.CancelKeyPress += (sender, eventArgs) =>
+{
+    flag = false;
+};
+while (flag)
+{
+    foreach (var dev in OneWireThermometerDevice.EnumerateDevices())
+    {
+        Console.WriteLine($"Name: {dev.DeviceId}");
+        Console.WriteLine($"Temperature: {dev.ReadTemperature():g}");
+        Console.WriteLine();
+    }
+}
